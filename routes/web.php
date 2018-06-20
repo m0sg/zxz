e50acd43d417dab7f7a982c1edc2d753dfe7859b
M0SG <m0sgsergey@gmail.com>
2018-06-20 13:00:25 +0300

    роуты и шаблон

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,57 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::match(['get', 'post'], '/','IndexController@execute');
+Route::get('/page/{alias}','PageController@execute');
+Route::auth();
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['prefix'=>'admin','middleware'=>'auth'], function() {
+    //admin
+    Route::get('/',function(){
+
+    });
+
+    //admin/pages
+    Route::group(['prefix'=>'pages'],function () {
+
+        //admin/pages
+        Route::get('/', 'PagesController@execute' );
+
+        //admin/pages/add
+        Route::match(['get','post'], '/add', 'PagesAddController@execute');
+
+        //admin/pages/edit/2
+        Route::match(['get','post','delete'], '/edit/{page}', 'PagesEditController@execute');
+
+    });
+
+    //admin/post
+    Route::group(['prefix'=>'post'],function () {
+
+        //admin/post
+        Route::get('/', 'PostController@execute' );
+
+        //admin/post/add
+        Route::match(['get','post'], '/add', 'PostAddController@execute');
+
+        //admin/post/edit/2
+        Route::match(['get','post','delete'], '/edit/{post}', 'PostEditController@execute');
+
+    });
+
+    //admin/services
+    Route::group(['prefix'=>'services'],function () {
+
+        //admin/services
+        Route::get('/', 'ServicesController@execute' );
+
+        //admin/services/add
+        Route::match(['get','post'], '/add', 'ServicesAddController@execute');
+
+        //admin/services/edit/2
+        Route::match(['get','post','delete'], '/edit/{service}', 'ServicesEditController@execute');
+
+    });
+
 });
